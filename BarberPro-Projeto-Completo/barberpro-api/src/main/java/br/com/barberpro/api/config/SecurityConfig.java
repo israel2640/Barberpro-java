30b1f5cd6e8a72567b380076bd5961c73aa723e9
Israel Paz <95177192+israel2640@users.noreply.github.com>
2025-10-03 17:23:39 -0300

    Update SecurityConfig.java

--- BarberPro-Projeto-Completo/barberpro-api/src/main/java/br/com/barberpro/api/config/SecurityConfig.java
+++ BarberPro-Projeto-Completo/barberpro-api/src/main/java/br/com/barberpro/api/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://localhost:5500"));
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://localhost:5500", "https://barberpro-frontend.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
