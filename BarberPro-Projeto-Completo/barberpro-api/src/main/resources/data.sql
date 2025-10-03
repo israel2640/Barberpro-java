ed35f4f69f496cd775b8b669a893d169319e1331
Israel Paz <95177192+israel2640@users.noreply.github.com>
2025-10-03 19:30:24 -0300

    Create data.sql

---
+++ BarberPro-Projeto-Completo/barberpro-api/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO barbeiros (nome) VALUES ('Pedro'), ('Lucas');
+INSERT INTO servicos (nome, descricao, preco) VALUES
+('Corte Social', 'Corte tradicional, na tesoura e máquina, acabamento perfeito.', 25.00),
+('Corte Navalhado', 'Finalização com navalha para linhas mais precisas e definidas.', 30.00);
