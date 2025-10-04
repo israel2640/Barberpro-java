a71f9835c9660c0d905c05203570f291ab73268a
Israel Paz <95177192+israel2640@users.noreply.github.com>
2025-10-03 21:25:55 -0300

    Update SecurityConfig.java

--- BarberPro-Projeto-Completo/barberpro-api/src/main/java/br/com/barberpro/api/config/SecurityConfig.java
+++ BarberPro-Projeto-Completo/barberpro-api/src/main/java/br/com/barberpro/api/config/SecurityConfig.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager; // <-- AÑADIR IMPORT
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration; // <-- AÑADIR IMPORT
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; // <-- AÑADIR IMPORT
+import org.springframework.security.crypto.password.PasswordEncoder; // <-- AÑADIR IMPORT
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -33,10 +35,15 @@ public class SecurityConfig {
                 .build();
     }
 
-    // --- MÉTODO AÑADIDO PARA RESOLVER EL ERROR ---
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    // --- MÉTODO AÑADIDO PARA RESOLVER EL ERROR ---
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
     // ------------------------------------------
 
