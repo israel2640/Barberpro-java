4009603a1597dcfc96e485b2ab8a4cd535f63367
Israel Paz <95177192+israel2640@users.noreply.github.com>
2025-10-14 17:37:27 -0300

    Update SecurityConfig.java

--- BarberPro-Projeto-Completo/barberpro-api/src/main/java/br/com/barberpro/api/config/SecurityConfig.java
+++ BarberPro-Projeto-Completo/barberpro-api/src/main/java/br/com/barberpro/api/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults; // <-- 1. ADICIONE ESTE IMPORT
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -24,28 +26,26 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
+                .cors(withDefaults()) // <-- 2. ADICIONE ESTA LINHA PARA ATIVAR O CORS
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
-
+                    // Rotas públicas
                     req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                    req.requestMatchers(HttpMethod.POST, "/login/barbeiro").permitAll();
                     req.requestMatchers(HttpMethod.POST, "/clientes").permitAll();
                     req.requestMatchers("/api/dados/**").permitAll();
 
-
-
-                    req.requestMatchers(HttpMethod.POST, "/login/barbeiro").permitAll();
-
-
+                    // Rotas do Barbeiro
                     req.requestMatchers("/barbeiro/**").hasRole("BARBER");
-
-
                     req.requestMatchers(HttpMethod.PATCH, "/agendamentos/**/concluir").hasRole("BARBER");
 
-
+                    // Rota de deletar agendamento para Cliente ou Barbeiro
                     req.requestMatchers(HttpMethod.DELETE, "/agendamentos/**").hasAnyRole("USER", "BARBER");
 
+                    // Rota de Admin
                     req.requestMatchers("/admin/**").hasRole("ADMIN");
 
+                    // Bloqueia todo o resto
                     req.anyRequest().authenticated();
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
