d2e311ec6f590f3b91980f977c0a80dcd909e3ec
Israel Paz <95177192+israel2640@users.noreply.github.com>
2025-10-14 17:34:31 -0300

    Update CorsConfig.java

--- BarberPro-Projeto-Completo/barberpro-api/src/main/java/br/com/barberpro/api/config/CorsConfig.java
+++ BarberPro-Projeto-Completo/barberpro-api/src/main/java/br/com/barberpro/api/config/CorsConfig.java
@@ -9,10 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    // 2. ANOTAÇÃO @NonNull ADICIONADA AO PARÂMETRO "registry"
+
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://barberpro-frontend.onrender.com")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
